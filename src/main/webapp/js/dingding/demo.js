fdcb6e08687af9e36e43ed555ef4347e7d9bb6b9
kale <330790416@qq.com>
2017-10-31 17:09:24 +0800

    完善2

--- src/main/webapp/js/dingding/demo.js
+++ src/main/webapp/js/dingding/demo.js
@@ -11,10 +11,11 @@ dd.config({
 			timeStamp : timeStamp,
 			nonceStr : nonceStr,
 			signature : signature,
+			type:0,
 			jsApiList : [ 'runtime.info', 'biz.contact.choose',
 					'device.notification.confirm', 'device.notification.alert',
 					'device.notification.prompt', 'biz.ding.post',
-					'biz.util.openLink' ]
+					'biz.util.openLink','biz.util.scan','biz.util.uploadImageFromCamera' ]
 		});
 
 
@@ -33,11 +34,11 @@ dd.ready(function() {
 				success : function(data, status, xhr) {
 				    alert(data);
 					var info = JSON.parse(data);
-					// if(info.isok == '1'){
-					// 	window.location.href=url_page+"/html/dingdingcache.jsp";
-					// }else{
-                     //    window.location.href=url_page+"/html/error/error.jsp";
-					// }
+					if(info.isok == '1'){
+						window.location.href=url_page+"/html/dingdingcache.jsp";
+					}else{
+                        window.location.href=url_page+"/html/error/error.jsp";
+					}
 
 				},
 				error : function(xhr, errorType, error) {
