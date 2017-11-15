d5f200212b88d52f6f4bbbb178d7515ae21fe8c4
xv330790416 <5287xv123456>
2017-11-15 14:42:55 +0800

    test

--- src/main/webapp/js/dingding/demo1.js
+++ src/main/webapp/js/dingding/demo1.js
@@ -22,38 +22,54 @@ dd.config({
 
 dd.ready(function() {
 
-    dd.device.geolocation.get({
-        targetAccuracy : 200,
-        coordinate : 1,
-        withReGeocode : false,
-        useCache:false, //默认是true，如果需要频繁获取地理位置，请设置false
-        onSuccess : function(result) {
-        	alert(result);
-            /* 高德坐标 result 结构
-            {
-                longitude : Number,
-                latitude : Number,
-                accuracy : Number,
-                address : String,
-                province : String,
-                city : String,
-                district : String,
-                road : String,
-                netType : String,
-                operatorType : String,
-                errorMessage : String,
-                errorCode : Number,
-                isWifiEnabled : Boolean,
-                isGpsEnabled : Boolean,
-                isFromMock : Boolean,
-                provider : wifi|lbs|gps,
-                accuracy : Number,
-                isMobileEnabled : Boolean
-            }
-            */
-        },
-        onFail : function(err) {}
-    });
+	dd.biz.util.uploadImage({
+		compression:true,//(是否压缩，默认为true)
+		onSuccess : function(result) {
+			alert("ok");
+			//onSuccess将在图片上传成功之后调用
+			/*
+			 [
+			 'http://gtms03.alicdn.com/tps/i3/TB1VF6uGFXXXXalaXXXmh5R_VXX-237-236.png'
+			 ]
+			 */
+		},
+		onFail : function(err) {
+			alert("error");
+		}
+	});
+
+    // dd.device.geolocation.get({
+    //     targetAccuracy : 200,
+    //     coordinate : 1,
+    //     withReGeocode : false,
+    //     useCache:false, //默认是true，如果需要频繁获取地理位置，请设置false
+    //     onSuccess : function(result) {
+    //     	alert(result);
+    //         /* 高德坐标 result 结构
+    //         {
+    //             longitude : Number,
+    //             latitude : Number,
+    //             accuracy : Number,
+    //             address : String,
+    //             province : String,
+    //             city : String,
+    //             district : String,
+    //             road : String,
+    //             netType : String,
+    //             operatorType : String,
+    //             errorMessage : String,
+    //             errorCode : Number,
+    //             isWifiEnabled : Boolean,
+    //             isGpsEnabled : Boolean,
+    //             isFromMock : Boolean,
+    //             provider : wifi|lbs|gps,
+    //             accuracy : Number,
+    //             isMobileEnabled : Boolean
+    //         }
+    //         */
+    //     },
+    //     onFail : function(err) {}
+    // });
 });
 
 dd.error(function(err) {
